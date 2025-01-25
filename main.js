0e4ebd355d99f9c8766aff10f5aee96c728978d2
Real-Proxy <113766584+Real-Proxy@users.noreply.github.com>
2025-01-26 00:16:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function checkAnswer() {
     const result = document.getElementById('result');
     
     if (userAnswer === hiddenAnswer || userAnswer === hiddenAnswer.replace(" ", "")) {
-        result.innerHTML = '<p class="success">Correct! Proceed to the next step (Insert reversed link here).</p>';
+        result.innerHTML = '<p class="success"> /2-level-elddiR/oi.buhtig.yxorp-laer//:sptth .</p>';
     } else {
         result.innerHTML = '<p class="error">Nope, wrong answer.</p>';
     }
